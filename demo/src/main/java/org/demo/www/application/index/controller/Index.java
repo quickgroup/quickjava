0ac042228ee91503b75cd43b455332f3812e4d20
qlopc-msi <qlopc-msi@zocell.com>
2021-01-21 20:21:02 +0800

    #QJ-14 截图

--- demo/src/main/java/org/demo/www/application/index/controller/Index.java
+++ demo/src/main/java/org/demo/www/application/index/controller/Index.java
@@ -4,58 +4,9 @@ import org.quickjava.framework.controller.Controller;
 
 public class Index extends Controller {
 
-    public Object view()
-    {
-        Index index = new Index();
-        return super.view();
-    }
-
     public Object index()
     {
-        StringBuffer output = new StringBuffer();
-        output.append("<h2>QuickJava page.</h2>");
-        output.append("<br>");
-        output.append(this.request.query());
-        output.append("<br>");
-        return output;
-    }
-
-    public Object test()
-    {
-        StringBuffer output = new StringBuffer();
-        output.append("Hello, world!");
-        output.append("<br>");
-        output.append("<h2>This QuickJava.</h2>");
-        output.append("<br>");
-        output.append(this.request.url);
-        output.append("<br>");
-        output.append(this.request.method);
-        output.append("<br>");
-        output.append("domin: " + this.request.domin);
-        output.append("<br>");
-        output.append(this.request.port);
-        output.append("<br>");
-        output.append(this.request.path);
-        output.append("<br>");
-        output.append(this.request.ip);
-        output.append("<br>");
-        output.append(this.request.moduleName);
-        output.append("<br>");
-        output.append(this.request.controllerName);
-        output.append("<br>");
-        output.append(this.request.actionName);
-        output.append("<br>");
-        return output;
-    }
-
-    public Object indexTest()
-    {
-        return "Hello, indexTest!";
-    }
-
-    private Object indexPrivate()
-    {
-        return "Hello, indexPrivate!";
+        return "QuickJava: Hello, World!";
     }
 
 }
---
+++ demo/src/main/java/org/demo/www/application/index/controller/Test.java
@@ -0,0 +1,60 @@
+package org.demo.www.application.index.controller;
+
+import org.quickjava.framework.controller.Controller;
+
+public class Test extends Controller {
+
+    public Object view()
+    {
+        return super.view();
+    }
+
+    public Object index()
+    {
+        StringBuffer output = new StringBuffer();
+        output.append("<h2>QuickJava page.</h2>");
+        output.append("<br>");
+        output.append(this.request.query());
+        output.append("<br>");
+        return output;
+    }
+
+    public Object test()
+    {
+        StringBuffer output = new StringBuffer();
+        output.append("Hello, world!");
+        output.append("<br>");
+        output.append("<h2>This QuickJava.</h2>");
+        output.append("<br>");
+        output.append(this.request.url);
+        output.append("<br>");
+        output.append(this.request.method);
+        output.append("<br>");
+        output.append("domin: " + this.request.domin);
+        output.append("<br>");
+        output.append(this.request.port);
+        output.append("<br>");
+        output.append(this.request.path);
+        output.append("<br>");
+        output.append(this.request.ip);
+        output.append("<br>");
+        output.append(this.request.moduleName);
+        output.append("<br>");
+        output.append(this.request.controllerName);
+        output.append("<br>");
+        output.append(this.request.actionName);
+        output.append("<br>");
+        return output;
+    }
+
+    public Object indexTest()
+    {
+        return "Hello, indexTest!";
+    }
+
+    private Object indexPrivate()
+    {
+        return "Hello, indexPrivate!";
+    }
+
+}
