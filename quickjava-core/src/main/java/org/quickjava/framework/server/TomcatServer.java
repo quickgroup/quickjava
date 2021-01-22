200ed87356c3864bca822ca2752c4a43f5ef0925
qlopc-msi <qlopc-msi@zocell.com>
2021-01-23 01:45:52 +0800

    #QJ-14 解决linux环境监听再ipv6的问题

--- quickjava-core/src/main/java/org/quickjava/framework/server/TomcatServer.java
+++ quickjava-core/src/main/java/org/quickjava/framework/server/TomcatServer.java
@@ -1,28 +1,43 @@
 package org.quickjava.framework.server;
 
+import org.apache.catalina.Host;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.HostConfig;
+import org.apache.catalina.startup.HostRuleSet;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
+import org.quickjava.common.QLog;
 import org.quickjava.common.QUtils;
 import org.quickjava.framework.App;
 
 import javax.servlet.ServletException;
 import java.io.File;
+import java.util.Properties;
 
 public class TomcatServer {
 
     public static void run()
             throws LifecycleException, ServletException
     {
+        // IPV4
+        Properties properties = System.getProperties();
+        properties.setProperty("java.net.preferIPv4Stack", "true");
+
         Tomcat tomcat = new Tomcat();
         /**
          * @langCn configure tomcat hostname and port
          */
         tomcat.setHostname(App.config.get("app").getString("hostname", "localhost"));
         tomcat.setPort(App.config.get("app").getInteger("port", 8080));
+        tomcat.getConnector().setUseIPVHosts(true);
+
+        QLog.info("Server Listen: "
+                + App.config.get("app").getString("hostname", "localhost")
+                + ":"
+                + App.config.get("app").getInteger("port", 8080));
 
         String classesPath = (QUtils.isClassMode()) ? QUtils.getClassesPath() : "";
         String webInfClasses = "/WEB-INF/classes";
