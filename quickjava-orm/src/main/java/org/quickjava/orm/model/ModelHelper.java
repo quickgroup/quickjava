769b07b77b9480800f62c7256138b3d0beff24f7
qlopc-tbk@zocell.com <qlopc-tbk@zocell.com>
2024-06-04 18:02:20 +0800

    orm-1.3# 修复模型追加表名处理了原始语句

--- quickjava-orm/src/main/java/org/quickjava/orm/model/ModelHelper.java
+++ quickjava-orm/src/main/java/org/quickjava/orm/model/ModelHelper.java
@@ -235,8 +235,12 @@ public class ModelHelper extends SqlUtil {
 
     // 回调处理方法
     public static WhereOptCallback whereOptCallback = (where, querySet, userData) -> {
-        // 子查询条件不处理
+        // 子查询条件跳过
         if (where.getChildren() != null && !where.getChildren().isEmpty()) {
+            return;
+        }
+        // 原始语句跳过
+        if (where.getOperator() == Operator.RAW) {
             return;
         }
         where.setColumn(fieldToUnderlineCase(where.getColumn()));
