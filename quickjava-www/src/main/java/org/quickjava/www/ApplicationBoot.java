007ef96f1c0b8b426e8b05fc65ea04103b9ae22a
qlopc-msi <qlopc-msi@zocell.com>
2025-03-14 00:50:19 +0800

    原生查询

--- quickjava-www/src/main/java/org/quickjava/www/ApplicationBoot.java
+++ quickjava-www/src/main/java/org/quickjava/www/ApplicationBoot.java
@@ -1,18 +1,17 @@
 package org.quickjava.www;
 
-import org.quickjava.web.framework.Kernel;
+import org.quickjava.web.framework.QuickJavaBoot;
 import org.quickjava.web.framework.annotation.ApplicationQuickBoot;
-import org.quickjava.www.test.TestDb;
 
 @ApplicationQuickBoot
 public class ApplicationBoot {
 
     public static void main(String[] args)
     {
-        Kernel.init(ApplicationBoot.class, args);
-        new TestDb().testQueryOOM();
+//        Kernel.init(ApplicationBoot.class, args);
+//        new TestDb().testQueryOOM();
 //        new TestDb().testTransactionOOM();
-//        QuickJavaBoot.start(ApplicationBoot.class, args);
+        QuickJavaBoot.start(ApplicationBoot.class, args);
     }
 
 }
